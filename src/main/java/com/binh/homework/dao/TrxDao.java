64e70f81f78e38f0bbf6df2e4b76b605b43b1183
binh <binh1990@126.com>
2017-04-21 21:47:48 +0800

    1

--- src/main/java/com/binh/homework/dao/TrxDao.java
+++ src/main/java/com/binh/homework/dao/TrxDao.java
@@ -12,17 +12,17 @@ import java.util.List;
  */
 public interface TrxDao {
 
-    @Select("select * from Trx where contentId=#{contentId} and personId=#{personId}")
+    @Select("select * from trx where contentId=#{contentId} and personId=#{personId}")
     public List<Trx> getTrxByContentIdPersonId(@Param("contentId") int contentId, @Param("personId") int personId);
 
 
-    @Select("select count(*) from Trx where contentId=#{contentId}")
+    @Select("select count(*) from trx where contentId=#{contentId}")
     public int getCountByContentId(@Param("contentId") int contentId);
 
-    @Select("select count(*) from Trx where contentId=#{contentId} and personId=#{personId}")
+    @Select("select count(*) from trx where contentId=#{contentId} and personId=#{personId}")
     public int getCountByContentIdPersonId(@Param("contentId") int contentId, @Param("personId") int personId);
 
-    @Select("select * from Trx where personId=#{personId}")
+    @Select("select * from trx where personId=#{personId}")
     public List<Trx> getTrxByPersonId(@Param("personId") int personId);
 
     @Insert("insert into trx (contentId, personId, price, time) values (#{contentId}, #{personId}, #{price}, #{time})")
